0915d499177dc6fcf926b19a33df610c32574cdb
mdjcole <mdjcole@gmail.com>
2017-03-03 13:38:42 +0100

    test again

--- sample.c
+++ sample.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-printf("hello world");
-printf("so long world");
+printf("hello world: how are you today?");
+printf("so long world - and thanks for all the fish");
 return 0;
 }
